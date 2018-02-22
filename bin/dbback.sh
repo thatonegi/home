f950815ae4c78b837a421339101526ab2174065d
thatonegi <bljohnston@gmail.com>
2018-02-22 15:53:21 -0800

    updated dbback to push backkups to git

--- bin/dbback.sh
+++ bin/dbback.sh
@@ -7,14 +7,15 @@ LPATH=root
 ARGS='--flush-logs --flush-privileges --complete-insert --create-options'
 SCHEMAS='MyMusic52 MyVideos93 alluc metadata cacti'
 BDIR=/home/bjohnston/Backups/mysql
-CHOWN=/bin/chown
-LUID=1000
-LGID=1000
 
 for SCHEMA in $SCHEMAS
 do
 	MDY=`date +'%m%d%y-%I%H%S%p'`
 	/usr/bin/mysqldump --login-path=$LPATH $ARGS $SCHEMA | $GZIP > $BDIR/$SCHEMA-$MDY.sql.gz
-	#${CHOWN} ${LUID}:${LGID} ${BDIR}/${SCHEMA}-${MDY}.sql.gz
+	/usr/bin/git add $BDIR/$SCHEMA-$MDY.sql.gz
 done
+
+/usr/bin/git commit -m "MySQL backup additions."
+/usr/bin/git push > /dev/null 2>&1
+
 exit
